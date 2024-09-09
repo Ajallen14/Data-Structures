41f66cf622512bc267ebdbf991eae985280d532a
Allen Jude <ajallen142001@gmail.com>
2024-09-09 20:19:37 +0530

    Commit

--- Queue/priority_queue.c
+++ Queue/priority_queue.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure to represent a priority queue element
 typedef struct {
     int data;
     int priority;
 } Element;
 
-// Structure to represent a priority queue
 typedef struct {
     Element *array;
     int size;
     int capacity;
 } PriorityQueue;
 
-// Function to create a new priority queue
 PriorityQueue* createPriorityQueue(int capacity) {
     PriorityQueue* queue = (PriorityQueue*) malloc(sizeof(PriorityQueue));
     queue->array = (Element*) malloc(sizeof(Element) * capacity);
@@ -23,17 +20,14 @@ PriorityQueue* createPriorityQueue(int capacity) {
     return queue;
 }
 
-// Function to check if the priority queue is empty
 int isEmpty(PriorityQueue* queue) {
     return queue->size == 0;
 }
 
-// Function to check if the priority queue is full
 int isFull(PriorityQueue* queue) {
     return queue->size == queue->capacity;
 }
 
-// Function to insert an element into the priority queue
 void enqueue(PriorityQueue* queue, int data, int priority) {
     if (isFull(queue)) {
         printf("Queue is full\n");
@@ -44,7 +38,6 @@ void enqueue(PriorityQueue* queue, int data, int priority) {
     newElement.data = data;
     newElement.priority = priority;
 
-    // Shift elements to the right to maintain the priority order
     for (int i = queue->size; i > 0; i--) {
         if (queue->array[i - 1].priority < priority) {
             queue->array[i] = queue->array[i - 1];
@@ -57,14 +50,12 @@ void enqueue(PriorityQueue* queue, int data, int priority) {
     queue->size++;
 }
 
-// Function to delete an element from the priority queue
 void dequeue(PriorityQueue* queue) {
     if (isEmpty(queue)) {
         printf("Queue is empty\n");
         return;
     }
 
-    // Shift elements to the left to fill the gap
     for (int i = 0; i < queue->size - 1; i++) {
         queue->array[i] = queue->array[i + 1];
     }
@@ -73,7 +64,7 @@ void dequeue(PriorityQueue* queue) {
     printf("Element is removed");
 }
 
-// Function to display the priority queue
+
 void display(PriorityQueue* queue) {
     for (int i = 0; i < queue->size; i++) {
         printf("Data: %d, Priority: %d\n", queue->array[i].data, queue->array[i].priority);
