d1e92d4664e99877024f8affaebe027c5000471f
Allen Jude <ajallen142001@gmail.com>
2024-10-03 19:47:55 +0530

    commit

--- Linked Lists/Polynomial Addition/poly_add.c
+++ Linked Lists/Polynomial Addition/poly_add.c
@@ -16,5 +16,36 @@ struct Node* createNode(int c, int p){
 }
 
 struct Node* addPoly(struct Node* head1, struct Node* head2){
+    struct Node* temp = createNode(0, 0);
+    struct Node* prev = temp; 
+    struct Node* cur1 = head1;
+    struct Node* cur2 = head2;
+
+    while (cur1 != NULL && cur2!= NULL){
+        if(cur1->pow < cur2->pow){
+            prev->next = cur2;
+            prev = cur2;
+            cur2 = cur2->next;
+        }else if(cur1->pow > cur2->pow){
+            prev->next = cur1;
+            prev = cur1;
+            cur1 = cur1->pow;
+        }else{
+            cur1->coeff = cur1->coeff + cur2->coeff;
+            prev->next = cur1;
+            prev = cur1;
+            cur1 = cur1->next;
+            cur2 = cur2->next;
+        }
+    }
+
+    if(cur1 != NULL){
+        prev->next = cur1;
+    }
+
+    if(cur1 != NULL){
+        prev->next = cur2;
+    }
     
+    return temp->next;
 }
