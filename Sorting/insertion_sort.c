dcabf4f8d0062debcad34f737010fbe2e30357e7
Allen Jude <ajallen142001@gmail.com>
2024-11-03 23:38:46 +0530

    Commit

--- Sorting/insertion_sort.c
+++ Sorting/insertion_sort.c
@@ -4,8 +4,7 @@
 
 void insertionSort(int arr[], int n){
 	int i, key, j;
-	for (i = 1; i < n; i++) 
-	{
+	for (i = 1; i < n; i++) {
 		key = arr[i];
 		j = i - 1;
 		while (j >= 0 && arr[j] > key) 
@@ -19,8 +18,9 @@ void insertionSort(int arr[], int n){
 
 void printArray(int arr[], int n){
 	int i;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++){
 		printf("%d ", arr[i]);
+    }
 	printf("\n");
 }
 
@@ -28,7 +28,7 @@ int main(){
 	int arr[100],n,i;
 	printf("enter no of elements : ");
 	scanf("%d",&n);
-    	printf("enter the elements :- \n");
+    printf("enter the elements :- \n");
 	for(i=0;i<n;i++){
 	    scanf("%d",&arr[i]);
 	}
