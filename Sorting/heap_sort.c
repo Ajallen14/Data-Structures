7b17d9d897256e3a8ce8c265371b5acfa0ed0311
Allen Jude <ajallen142001@gmail.com>
2024-11-03 23:59:37 +0530

    Commit

--- Sorting/heap_sort.c
+++ Sorting/heap_sort.c
@@ -5,45 +5,49 @@ void swap(int* a, int* b){
 	*a = *b;
 	*b = temp;
 }
-void heapify(int arr[], int N, int i)
-{
+
+void heapify(int arr[], int N, int i){
 	int largest = i;
 	int left = 2 * i + 1;
 	int right = 2 * i + 2;
-	if (left < N && arr[left] > arr[largest])
+	if (left < N && arr[left] > arr[largest]){
 		largest = left;
-	if (right < N && arr[right] > arr[largest])
+    }
+	if (right < N && arr[right] > arr[largest]){
 		largest = right;
+    }
 	if (largest != i) {
 		swap(&arr[i], &arr[largest]);
 		heapify(arr, N, largest);
 	}
 }
-void heapSort(int arr[], int N)
-{
-	for (int i = N / 2 - 1; i >= 0; i--)
+
+void heapSort(int arr[], int N){
+	for (int i = N / 2 - 1; i >= 0; i--){
 		heapify(arr, N, i);
+    }
 	for (int i = N - 1; i >= 0; i--) {
 		swap(&arr[0], &arr[i]);
 		heapify(arr, i, 0);
 	}
 }
-void printArray(int arr[], int N)
-{
-	for (int i = 0; i < N; i++)
+
+void printArray(int arr[], int N){
+	for (int i = 0; i < N; i++){
 		printf("%d ", arr[i]);
+    }
 	printf("\n");
 }
-int main()
-{
+
+int main(){
 	int arr[100],n,i;
-	printf("enter no of elements:");
+	printf("enter no of elements : ");
 	scanf("%d",&n);
+    printf("enter the elements :\n");
 	for(i=0;i<n;i++){
-	    printf("enter:");
 	    scanf("%d",&arr[i]);
 	}
 	heapSort(arr, n);
-	printf("Sorted array is\n");
+	printf("Sorted array is :-\n");
 	printArray(arr, n);
 }
