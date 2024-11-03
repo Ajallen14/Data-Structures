e7c5abdd2251edf4448d8b093f53e76b96c1b563
Allen Jude <ajallen142001@gmail.com>
2024-11-03 23:27:27 +0530

    Commit

---
+++ Sorting/bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[20],a,i,j,temp;
+    printf("number of values u want to input:-");
+    scanf("%d",&a);
+    for(i=0;i<a;i++){
+        printf("enter value no %d:-",i+1);
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<a-1;i++){
+        for(j=0;j<a-i-1;j++){
+            if (arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;   
+                
+            }//printf("%d,",arr[j]);
+        }
+    }
+    printf("[");
+    for(i=0;i<a;i++){
+        printf("%d,",arr[i]);
+    }
+    printf("]");
+
+    return 0;
+}
